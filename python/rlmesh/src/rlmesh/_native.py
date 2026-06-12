7e404d90c3b0fd19daac094b4a5297d300f1eed9
Josh Blayone <josh@blay.one>
2026-06-12 10:40:19 -0400

    refactor(python): drop unreachable _native sandbox classes

--- python/rlmesh/src/rlmesh/_native.py
+++ python/rlmesh/src/rlmesh/_native.py
@@ -7,7 +7,6 @@ from typing import ClassVar, final
 from ._values import ValueBridge, identity_bridge
 from .client import RemoteEnvBase, RemoteVectorEnvBase
 from .model import ModelBase
-from .sandbox import SandboxEnvBase, SandboxInfo, SandboxVectorEnvBase
 from .types import Value
 
 
@@ -26,21 +25,8 @@ class Model(ModelBase[Value, Value]):
     _bridge: ClassVar[ValueBridge] = identity_bridge
 
 
-@final
-class SandboxEnv(SandboxEnvBase[Value, Value]):
-    _remote_env_cls = RemoteEnv
-
-
-@final
-class SandboxVectorEnv(SandboxVectorEnvBase[Value, Value]):
-    _remote_env_cls = RemoteVectorEnv
-
-
 __all__ = [
     "Model",
     "RemoteEnv",
     "RemoteVectorEnv",
-    "SandboxEnv",
-    "SandboxInfo",
-    "SandboxVectorEnv",
 ]
